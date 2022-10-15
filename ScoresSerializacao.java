68f7deaab69d9af0cab416cacd205d6511798bc3
MariaContu <112641497+MariaContu@users.noreply.github.com>
2022-10-15 11:48:15 -0300

    Add files via upload

---
+++ ScoresSerializacao.java
@@ -0,0 +1,10 @@
+/*Implemente uma classe utilitária
+ * ScoresSerializacaocom métodos para
+ * ler e escrever os dados da coleção
+ * de scores em arquivoscom o seguinte formato 
+ * “comma separated values”, https://pt.wikipedia.org/wiki/Comma-separated_values
+*/
+
+public class ScoresSerializacao {
+    
+}
